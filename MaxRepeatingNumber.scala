69c6ad3b26fcf558ecd78d5c26de5c5ada01687d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-16 23:48:59 +0530

    Finding Maximum Repeating Element in O(N) Time and O(1) Space Complexity

--- MaxRepeatingNumber.scala
+++ MaxRepeatingNumber.scala
@@ -1,18 +1,30 @@
 object MaxRepeatingNumber extends App {
-  def findMaxRepeatingNumber(numbers: List[Int]): Option[Int] = {
-    val countMap = scala.collection.mutable.HashMap[Int, Int]().withDefaultValue(0)
-    for (num <- numbers) {
-      countMap(num) += 1
+    def findMaxRepeatingNumber(numbers: Array[Int]): Option[Int] = {
+      val n = numbers.length
+      for (i <- 0 until n) {
+        numbers(numbers(i) % n) += n
+      }  
+      var maxIndex = 0
+      for (i <- 1 until n) {
+        if (numbers(i) > numbers(maxIndex)) {
+          maxIndex = i
+        }
+      }
+      for (i <- 0 until n) {
+        numbers(i) %= n
+      }
+      if (numbers(maxIndex) > 1) {
+        Some(maxIndex)
+      } else {
+        None
+      }
     }
-    val maxRepeatingNumber = countMap.maxBy(_._2)._1
-    Some(maxRepeatingNumber)
-  }
-  val numbers = List(1, 2, 3, 4, 2, 2, 5, 6, 2, 7, 8, 2, 9, 10, 2)
-  val result = findMaxRepeatingNumber(numbers)
-  result match {
-    case Some(maxRepeatingNumber) =>
-      println(s"The maximum repeating number is => $maxRepeatingNumber")
-    case None =>
-      println("No repeating numbers found.")
-  }
+    val numbers = Array(1, 2, 3, 4, 2, 2, 5, 6, 2, 7, 8, 2, 9, 10, 2)
+    val result = findMaxRepeatingNumber(numbers)
+    result match {
+      case Some(maxRepeatingNumber) =>
+        println(s"The maximum repeating number is => $maxRepeatingNumber")
+      case None =>
+        println("No repeating numbers found.")
+    }
 }
