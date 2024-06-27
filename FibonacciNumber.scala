388ee62f68d993ab521b0fe315e2dfa352c2b211
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-06-28 00:54:47 +0530

    Fibonacci Number Using Recursion in Scala

--- FibonacciNumber.scala
+++ FibonacciNumber.scala
@@ -1,17 +1,7 @@
 object FibonacciNumber {
   def fibonacci(n: Int): Int = {
-    if (n <= 1) return n
-    
-    var a = 0
-    var b = 1
-    var i = 2
-    while (i <= n) {
-      val c = a + b
-      a = b
-      b = c
-      i += 1
-    }
-    b
+    if (n <= 1) n
+    else fibonacci(n - 1) + fibonacci(n - 2)
   }
   
   def main(args: Array[String]): Unit = {
